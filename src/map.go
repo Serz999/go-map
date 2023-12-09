300dcaca98d1a891443b0221cc65cab4119210a2
serz999 <serz99992@gmail.com>
2023-12-09 21:10:17 +0300

    feat: implement storege interface

--- src/map.go
+++ src/map.go
@@ -1,7 +1,9 @@
 package customMap
 
 import (
+	"errors"
 	"fmt"
+	"reflect"
 )
 
 type Storage interface {
@@ -15,15 +17,18 @@ type Storage interface {
     Print()
 }
 
+
+var ErrMismatchType = errors.New("mismatched type: the type of the provided value does not match the type of items already in the storage")
+
 type Map struct {
     nextIndex int64
-    mp map[int64]int64
+    mp map[int64]any
 }
 
 func NewMap() (mp *Map) {
     return &Map{
         nextIndex: 0,
-        mp: make(map[int64]int64),
+        mp: make(map[int64]any),
     }
 }
 
@@ -31,17 +36,24 @@ func (this *Map) Len() (l int64) {
     return int64(len(this.mp))
 }
 
-func (this *Map) Add(value int64) int64 {
+func (this *Map) Add(value any) (int64, error) {
+    if this.Len() > 0 {
+        firstValue, _ := this.GetByIndex(0)
+        if reflect.TypeOf(firstValue) != reflect.TypeOf(value) {
+            return 0, ErrMismatchType 
+        }
+    }
+
     this.mp[this.nextIndex] = value
     this.nextIndex++
-    return this.nextIndex - 1
+    return this.nextIndex - 1, nil
 }
 
 func (this *Map) RemoveByIndex(id int64) {
     delete(this.mp, id)
 }
 
-func (this *Map) RemoveByValue(value int64) {
+func (this *Map) RemoveByValue(value any) {
     for k, v := range this.mp {
         if v == value {
             delete(this.mp, k)    
@@ -50,13 +62,20 @@ func (this *Map) RemoveByValue(value int64) {
     }
 }
 
-func (this *Map) GetByIndex(id int64) (int64, bool) {
+func (this *Map) RemoveAllByValue(value any) {
+    for k, v := range this.mp {
+        if v == value {
+            delete(this.mp, k)
+        }
+    }
+}
+
+func (this *Map) GetByIndex(id int64) (any, bool) {
     value, ok := this.mp[id]
     return value, ok
 }
 
-func (this *Map) GetByValue(value int64) (int64, bool) {
-
+func (this *Map) GetByValue(value any) (int64, bool) {
     for k, v := range this.mp {
         if v == value {
             return k, true
@@ -66,8 +85,34 @@ func (this *Map) GetByValue(value int64) (int64, bool) {
     return 0, false
 }
 
+func (this *Map) GetAllByValue(value any) (ids []int64, ok bool) { 
+    for k, v := range this.mp {
+        if v == value {
+            ids = append(ids, k)
+        }
+    }
+
+    if len(ids) > 0 {
+        ok = true
+    }
+
+    return ids, ok
+}
+
+func (this *Map) getAll() (values []any, ok bool) {
+    for _, v := range this.mp {
+        values = append(values, v) 
+    }
+
+    if len(values) > 0 {
+        ok = true
+    }
+
+    return
+}
+
 func (this *Map) Clear() {
-    this.mp = make(map[int64]int64)
+    this.mp = make(map[int64]any)
     this.nextIndex = 0
 }
 
